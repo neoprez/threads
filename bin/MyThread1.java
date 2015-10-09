dced32fad13b5182dc41a546c22a77e4eb4ab52f
Rodny Perez <rodnyeperez@gmail.com>
2015-10-09 00:33:29 -0400

    Basic threads with java

---
+++ bin/MyThread1.java
@@ -0,0 +1,16 @@
+class MyThread1 extends Thread {
+		public void run() {
+				try {
+						sleep(1000);
+				}
+				catch(InterruptedException ex) {
+						ex.printStackTrace();
+				}
+				System.out.println("In run method; thread name is: " + this.getName());
+		}
+		public static void main(String args[]){
+				Thread myThread = new MyThread1();
+				myThread.start();
+				System.out.println("In main method; thread name is: " + Thread.currentThread().getName());
+		}
+}

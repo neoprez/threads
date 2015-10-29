940b3d46688fff1b5d17d441065564c3c78a0938
Rodny Perez <rodnyeperez@gmail.com>
2015-10-29 14:11:27 -0400

    Implement blocking queue example

---
+++ bin/PriorityBlockingQueueExample.java
@@ -0,0 +1,28 @@
+// Illustrates the use of PriorityBlokingQueue. In this case, if there is no element available in the priority queue
+// the thread calling tak*( method will block (i.e., wait) until another thread inserts an element
+import java.util.concurrent.*;
+
+class PriorityBlockingQueueExample {
+		public static void main(String[] args) {
+				final PriorityBlockingQueue<Integer> priorityBlockingQueue = new PriorityBlockingQueue<>();
+				new Thread() {
+						public void run() {
+								try {
+										// use take() instead of remove()
+										// note that take() block, whereas remove() doesn't block
+										System.out.println("The removed element is: " + priorityBlockingQueue.take());
+								} catch(InterruptedException ie) {
+										ie.printStackTrace();
+								}
+						}
+				}.start();
+
+				new Thread() {
+						public void run() {
+								// add an element with value 10 to the priority queue
+								priorityBlockingQueue.add(10);
+								System.out.println("Successfully added an element to the queue");	
+						}
+				}.start();
+		}
+}

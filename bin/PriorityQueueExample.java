3ae7c2c769bdb59a9fe35b19ccdb919c30b915a8
Rodny Perez <rodnyeperez@gmail.com>
2015-10-29 14:01:12 -0400

    Add non-blocking Priority queue example

---
+++ bin/PriorityQueueExample.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+// Simple PriorityQueue example. Here, we create two threads in which one thread inserts an element,
+// and another thread removes and element from the priority queue.
+class PriorityQueueExample {
+		public static void main(String[] args) {
+				final PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+				// spawn a thread that removes an element from the priority queue
+
+				new Thread() {
+						public void run() {
+								// Use remove() method in PriorityQueue to remove the element if available
+								System.out.println("The removed element is: " + priorityQueue.remove());
+						}
+				}.start();
+				// spawn a thread that inserts an element into the priority queue
+				new Thread() {
+						public void run() {
+								// insert integer value 10 as an entry into the priority queue
+								priorityQueue.add(10);
+								System.out.println("Successfully added an element to the queue ");
+						}
+				}.start();
+		}
+}
